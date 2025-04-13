3c67b8c68f2cee46eb1fbf62c82cd1b4e5443a92
5wapnill <mswapnil383@gmail.com>
2025-04-13 17:44:42 +0530

    Refactor HTML and JavaScript for TextManipulator: add element IDs and event listeners

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-let uText = 
+let uText = document.querySelector("#uText");
+let lowerCase = document.querySelector("#lowerCase");
+let upperCase = document.querySelector("#upperCase");
+let inverseCase = document.querySelector("#inverseCase");
+let mixedCase = document.querySelector("#mixedCase");
+
+let clear = document.querySelector("#clear");
+
+lowerCase.addEventListener("click", () => {
+    uText.value = (uText.value).toLowerCase();
+    console.log(uText);
+});
+
+upperCase.addEventListener("click", () => {
+    uText.value = (uText.value).toUpperCase();
+    console.log(uText);
+});
+
+inverseCase.addEventListener("click", () => {
+    let uTextV = uText.value;
+    let temp = "";
+    for(let i = 0; i < uTextV.length; i++){
+        if(i % 2 == 1) {
+            temp += uTextV[i].toLowerCase();
+        } else {
+            temp += uTextV[i].toUpperCase();
+        }
+    }
+    uText.value = temp;
+});
+
+mixedCase.addEventListener("click", () => {
+    let uTextV = uText.value;
+    let temp = "";
+    for(let i = 0; i < uTextV.length; i++){
+        if((Math.random()*11) % 2 == 0) {
+            temp += uTextV[i].toLowerCase();
+        } else {
+            temp += uTextV[i].toUpperCase();
+        }
+    }
+    uText.value = temp;
+});
+
+clear.addEventListener("click", () => {
+    uText.value = "";
+})
