02c46ab4da004e63da0d086197c1f2c2942b5404
5wapnill <mswapnil383@gmail.com>
2025-05-16 19:38:13 +0530

    trying gsap

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
-let uText = document.querySelector("#uText");
-let lowerCase = document.querySelector("#lowerCase");
-let upperCase = document.querySelector("#upperCase");
-let inverseCase = document.querySelector("#inverseCase");
-let mixedCase = document.querySelector("#mixedCase");
-let titleCase = document.querySelector("#titleCase");
-
-let clear = document.querySelector("#clear");
-let copy = document.querySelector("#copy");
-
-lowerCase.addEventListener("click", () => {
-    uText.value = (uText.value).toLowerCase();
-    console.log(uText);
-});
-
-upperCase.addEventListener("click", () => {
-    uText.value = (uText.value).toUpperCase();
-    console.log(uText);
-});
-
-inverseCase.addEventListener("click", () => {
-    let uTextV = uText.value;
-    let temp = "";
-    for(let i = 0; i < uTextV.length; i++){
-        if(i % 2 == 1) {
-            temp += uTextV[i].toLowerCase();
-        } else {
-            temp += uTextV[i].toUpperCase();
+document.addEventListener("DOMContentLoaded", () => {
+    // Animate the heading with ScrambleText
+    gsap.to("#heading", {
+        duration: 2, // Animation duration
+        scrambleText: {
+            text: "Text Manipulator", // Final text to display
+            chars: "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()", // Characters to scramble with
+            revealDelay: 0.5 // Delay before revealing the final text
         }
-    }
-    uText.value = temp;
-});
+    });
+    
+    let uText = document.querySelector("#uText");
+    let lowerCase = document.querySelector("#lowerCase");
+    let upperCase = document.querySelector("#upperCase");
+    let inverseCase = document.querySelector("#inverseCase");
+    let mixedCase = document.querySelector("#mixedCase");
+    let titleCase = document.querySelector("#titleCase");
 
-mixedCase.addEventListener("click", () => {
-    let uTextV = uText.value;
-    let temp = "";
-    for(let i = 0; i < uTextV.length; i++){
-        if(parseInt(Math.random()*10) % 2 == 1) {
-            temp += uTextV[i].toLowerCase();
-        } else {
-            temp += uTextV[i].toUpperCase();
+    let clear = document.querySelector("#clear");
+    let copy = document.querySelector("#copy");
+
+    lowerCase.addEventListener("click", () => {
+        uText.value = (uText.value).toLowerCase();
+        console.log(uText);
+    });
+
+    upperCase.addEventListener("click", () => {
+        uText.value = (uText.value).toUpperCase();
+        console.log(uText);
+    });
+
+    inverseCase.addEventListener("click", () => {
+        let uTextV = uText.value;
+        let temp = "";
+        for(let i = 0; i < uTextV.length; i++){
+            if(i % 2 == 1) {
+                temp += uTextV[i].toLowerCase();
+            } else {
+                temp += uTextV[i].toUpperCase();
+            }
         }
-    }
-    uText.value = temp;
+        uText.value = temp;
+    });
+
+    mixedCase.addEventListener("click", () => {
+        let uTextV = uText.value;
+        let temp = "";
+        for(let i = 0; i < uTextV.length; i++){
+            if(parseInt(Math.random()*10) % 2 == 1) {
+                temp += uTextV[i].toLowerCase();
+            } else {
+                temp += uTextV[i].toUpperCase();
+            }
+        }
+        uText.value = temp;
+    });
+
+    titleCase.addEventListener("click", () => {
+        let uTextV = uText.value;
+        let temp = "";
+        uTextV.toLowerCase();
+        for(let i = 0; i < uTextV.length; i++){
+
+            if(uTextV[i] == " ")
+            {
+                uTextV[i+1] = uTextV[i+1].toUpperCase();
+                i++
+            } 
+        }
+        uText.value = uTextV;
+    });
+
+    clear.addEventListener("click", () => {
+        uText.value = "";
+    })
+    copy.addEventListener("click", () => {
+        navigator.clipboard.writeText(uText.value);
+    })
 });
-
-titleCase.addEventListener("click", () => {
-    let uTextV = uText.value;
-    let temp = "";
-    uTextV.toLowerCase();
-    for(let i = 0; i < uTextV.length; i++){
-
-        if(uTextV[i] == " ")
-        {
-            uTextV[i+1] = uTextV[i+1].toUpperCase();
-            i++
-        } 
-    }
-    uText.value = uTextV;
-});
-
-clear.addEventListener("click", () => {
-    uText.value = "";
-})
-copy.addEventListener("click", () => {
-    navigator.clipboard.writeText(uText.value);
-})
 
 
